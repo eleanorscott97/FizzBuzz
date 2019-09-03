11cfe051a910b92a1674fe58f472a596715e5db5
Eleanor Scott <Eleanor.Scott@softwire.com>
2019-09-03 17:29:15 +0100

    Part 1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzz
 {
@@ -6,7 +7,32 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //100 is the very special number given in the exercise
+            int total = 100;
+
+            for (int i = 1; i <= total; i++) 
+            {
+                // check for multiples of 3,5 and 3&5 - switch to Fizz, Buzz, FizzBuzz respectively
+                if(i % 3 == 0)
+                {
+                    if(i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
